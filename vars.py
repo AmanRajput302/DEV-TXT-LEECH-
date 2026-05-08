99c61be58305be1d47198d61f24dad3a93e50748
AmanRajput302 <minibloggeraman@gmail.com>
2026-05-08 20:35:13 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,7 +6,7 @@ API_ID = int(os.environ.get("API_ID", "23480065"))
 API_HASH = os.environ.get("API_HASH", "32edb7d7fc1523b436109bff8ea061fc")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7886834731:AAHnUn4rloTDZuzkoT2m5qHc2cHA1gCGvTQ")
 
-CREDIT = os.environ.get("CREDIT", "<a href='https://t.me/TGxThakur'>🕊️⃝🗽𝐀𝐬𝐡𝐮❥⟵🕊️⃝❤️</a>")
+CREDIT = os.environ.get("CREDIT", "<a href='https://t.me/TGxThakur'>🕊️⃝🗽𝐍𝐚𝐦𝐚𝐧⤷🕊️⃝❤️</a>")
 # MongoDB Configuration
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "DevThanos")
 DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://savebot:RxxBfvkv1xnGsbPL@cluster0.9zpw7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # Add your own atlas db
